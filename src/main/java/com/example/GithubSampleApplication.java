6ebf39199a8669ba3b4123cdf8677cfc046ab824
James <James@host.docker.internal>
2024-01-21 13:41:24 -0500

    Signed-off-by: James <James@host.docker.internal>

---
+++ src/main/java/com/example/GithubSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubSampleApplication.class, args);
+	}
+
+}
